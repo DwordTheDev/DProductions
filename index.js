851abf7153fd0042a764d1b7bdceaf985bb5d4f7
DWORD <69285131+DwordTheDev@users.noreply.github.com>
2020-08-22 11:42:24 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -9,14 +9,12 @@ const { join } = require("path");
 
 const { readFileSync } = require("fs");
 
-app.use("/api", require("./api"))
+app.use("/", require("./api"))
 
 app.use(express.static(__dirname));
 
 
-app.get("/",function(  req,res){
-    res.render("index");
-})
+
 
 app.listen(port,function(){
     console.log("oof")
